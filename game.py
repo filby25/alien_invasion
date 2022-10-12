bc0078639f7997f0c136309a9f8ca71562c5fbfd
filby25 <115638552+filby25@users.noreply.github.com>
2022-10-12 09:39:46 -0400

    start of game

--- game.py
+++ game.py
@@ -5,4 +5,10 @@ class AlienInvalsion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
-        """"""
+        """Initialize the game, and create game resources"""
+        pygame.init()
+
+        self.screen=pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    
